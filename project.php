af165b7e690ecba2a25c9abc72b5a3147a252393
zokshan <150640034+zokshan@users.noreply.github.com>
2023-12-24 19:17:30 +0200

    Update project.php

--- project.php
+++ project.php
@@ -3,8 +3,6 @@
         $watchType = isset($_POST["watch-type"]) ? $_POST["watch-type"] : '';
         $quantity = isset($_POST["quantity"]) ? $_POST["quantity"] : '';
     }
-        // Process the order, save to a database, etc.
-        // In a real-world scenario, you would perform more robust validation and store data securely.
 
         if ($watchType && $quantity) {
             echo "<p>Thank you for your order!</p>";
